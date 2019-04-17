4ace9068b19fe3df83f60a36d5e5faa9c487f213
kimetix99 <quimpm99@gmail.com>
2019-04-17 04:54:52 +0200

    send servidor amb errors

--- Servidor.py
+++ Servidor.py
@@ -1,3 +1,6 @@
+# coding=utf-8
+#enconding: utf-8
+
 import socket
 import sys
 import threading
@@ -9,10 +12,10 @@ from random import randint
 import time
 import os
 
-#enconding: utf-8
 x=0
 j=2
 k=3
+w=4
 
 def server_configuration():
     fitxer_configuracio=open("server.cfg", "r")
@@ -37,13 +40,17 @@ def get_clients_autoritzats():
     fitxer_clients = open("equips.dat", "r")
     llista_clients=[]
     for line in fitxer_clients:
-        client=Client(line.split()[0],line.split()[1],"DISCONNECTED","000000","",False,0)
+        client=Client(line.split()[0],line.split()[1],"DISCONNECTED","000000","",False,0,False)
         llista_clients.append(client)
         
     return llista_clients
 
 def data_treatment(data_struct):
     data=POINT.from_buffer_copy(data_struct)
+    return data
+
+def data_treatment_tcp(data_struct):
+    data=POINT_TCP.from_buffer_copy(data_struct)
     return data
 
 def debuger(msg):
@@ -62,7 +69,7 @@ def reply(data, addr, clients, server_config, sock):
         check_alive(data, addr, client, server_config, sock)
 
 def check_client(data,clients):
-    client=Client("0000000","0000000000000","DISCONNECTED","000000","",False,0)
+    client=Client("0000000","0000000000000","DISCONNECTED","000000","",False,0, False)
     for c in clients:
         if data.nom_equip==c.nom and data.mac_address==c.mac:
             client=c
@@ -190,8 +197,15 @@ class POINT (Structure):
                 ("num_aleatori",c_char*7),
                 ("dades",c_char*50)]
 
+class POINT_TCP (Structure):
+    _fields_ = [("tipus_paquet",c_ubyte),
+                ("nom_equip",c_char*7),
+                ("mac_address",c_char*13),
+                ("num_aleatori",c_char*7),
+                ("dades",c_char*150)]
+
 class Client:
-    def __init__(self,nom,mac,estat,aleatori,ip,accepted,ttl_alive):
+    def __init__(self,nom,mac,estat,aleatori,ip,accepted,ttl_alive,tcp_active):
         self.nom=nom
         self.mac=mac
         self.estat=estat
@@ -199,7 +213,72 @@ class Client:
         self.ip=ip
         self.accepted=accepted
         self.ttl_alive=ttl_alive
+        self.tcp_active=tcp_active
 
+
+def reply_tcp(client_tcp,sock_tcp,clients,server_config,addr_tcp):
+    #Agafem dades del canal TCP
+    data_struct_tcp = client_tcp.recv(178)
+    data_struct_tcp=data_treatment_tcp(data_struct_tcp)
+
+    #Comprovem que sigui un client autoritzat
+    client=check_client(data_struct_tcp,clients)
+
+    if(client.accepted==True and client.tcp_active==False):
+        client.tcp_active==True
+        check_tcp_pdu(data_struct_tcp,client,sock_tcp,server_config,addr_tcp)
+
+    else:
+        debuger("Enviat SEND_NACK, aquest client ja té el port tcp actiu")
+        data_send_tcp=POINT_TCP(tipus_paquet=0x22, nom_equip="", mac_address="0000000000000",  num_aleatori="000000", dades="Dades addiccional errònies")
+        sock_tcp.send(data_send_tcp)
+        sock_tcp.close()
+        return 0
+        
+
+def check_tcp_pdu(data_struct_tcp,client,sock_tcp,server_config,addr_tcp):
+    if(data_struct_tcp.tipus_paquet==0x20):
+        check_send_conf(data_struct_tcp,client,sock_tcp,server_config,addr_tcp)
+    if(data_struct_tcp.tipus_paquet==0x30):
+        #check_get_conf(data,client,sock_tcp,server_config,addr_tcp)
+        return 0
+
+def check_send_conf(data_struct_tcp,client,sock_tcp,server_config,addr_tcp):
+    if data_struct_tcp.nom_equip==client.nom and data_struct_tcp.mac_address==client.mac:
+        if data_struct_tcp.num_aleatori==client.aleatori and addr_tcp[0]==client.ip:
+            data_send_tcp=POINT_TCP(tipus_paquet=0x21, nom_equip=server_config["nom"], mac_address=server_config["mac"],  num_aleatori=client.aleatori, dades=client.nom+".cfg")
+            sock_tcp.send(data_send_tcp)
+            debuger("Enviat SEND_ACK")
+            file_tcp = open(client.nom+".cfg", "a")
+            while data.tipus_paquet!=0x25:
+                ready = select.select([sock_tcp], [], [], w)
+                if ready[0]:
+                    data_recv_tcp=sock_tcp.recv(178)
+                data_recv_tcp=data_treatment(data_recv_tcp)
+                file_tcp.write(data_recv_tcp.dades)
+            debuger("Enviats tots els SEND_DATA")
+            file_tcp.close()
+            sock_tcp.close()
+            client.tcp_active==False
+            return 0
+        else:
+            debuger("Dades addiccional errònies ")
+            data_send_tcp=POINT_TCP(tipus_paquet=0x22, nom_equip="", mac_address="0000000000000",  num_aleatori="000000", dades="Dades addiccional errònies")
+            sock_tcp.send(data_send_tcp)
+            debuger("Enviat SEND_NACK")
+            sock_tcp.close()
+            client.tcp_active==False
+            return 0
+    else:
+        debuger("Dades principals errònies")
+        data_send_tcp=POINT_TCP(tipus_paquet=0x23, nom_equip="", mac_address="0000000000000",  num_aleatori="000000", dades="Dades principals errònies")
+        sock_tcp.send(data_send_tcp)
+        debuger("Enviar SEND_REJ")
+        sock_tcp.close()
+        client.tcp_active==False
+        return 0
+
+                
 
 if __name__=='__main__':
     debuger("Inici del servidor")
@@ -207,16 +286,25 @@ if __name__=='__main__':
     clients=get_clients_autoritzats()
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.bind(("localhost", int(server_config["udp_port"])))
-    debuger("Socket creat")
+    debuger("Socket UDP creat")
+    sock_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock_tcp.bind(("localhost", int(server_config["tcp_port"])))
+    listen=sock_tcp.listen(5)
+    debuger("Socket TCP creat") 
     t_listen=threading.Thread(target=listen, args=(clients,))
     t_listen.daemon=True
     t_listen.start()
     while True:
+        #UDP
         data_struct, addr = sock.recvfrom(78)
         data=data_treatment(data_struct)
         server_config=server_configuration()
         reply(data, addr, clients, server_config, sock)
-        
-
-
-
+        #TCP 
+        prepared = select.select([sock_tcp], [], [], 0)
+        if prepared[0]:
+            client_tcp,addr_tcp = sock_tcp.accept()
+            debuger("Creat fill per a gestio del port TCP")
+            t_listen_tcp=threading.Thread(target=reply_tcp, args=(client_tcp,sock_tcp,clients,server_config,addr_tcp,))
+            t_listen_tcp.daemon=True
+            t_listen_tcp.start()
